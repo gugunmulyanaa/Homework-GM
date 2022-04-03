b2bd3aad08fb34d2cc6290045e78b88951c256e1
gugunmulyanaa <gugunmulyana7@gmail.com>
2022-04-03 12:45:44 +0700

    modul 3-3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import data from './Data/albums';
-import Cards from './Component/Card';
-import Search from './Component/Search';
 import Cards from './Component/Card'; import List from './Component/List'; import Search from './Component/Search';
 import Auth from './Page/auth';
 import Container from 'react-bootstrap/esm/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import List from './Component/List';
 import Col from 'react-bootstrap/Col'; import Row from 'react-bootstrap/Row';
 import {useState, useEfect} from 'react' ;
 import  axios from 'axios';
+// import  axios from 'axios';
 
 
 function App() {
@@ -18,24 +14,28 @@ function App() {
   [accToken, setAccToken] = useState();
   [accesSrcKey, setAccToken] = useState();
   [accesTrack, setAccTrack] = useState();
-  [isAuth, setAuth] = useState(false);
+@@ -28,13 +27,15 @@ function App() {
 
-  useEfect(() => {
-    if(window.location.hash) {
-      const {access_token} = getParamsFromUrl(window.location.hash)
-      setAccToken(access_token)
-    }
-    if (access_token) {
-      setAuthenticaed(true)
-    }
-  }, [access_token])
-
-
-function App() {
   return (
     <div className="App">
       <Auth />
-@@ -44,3 +58,4 @@ function App() {
-}
 
-export default App;
+      <Container>
+
+      <Search />
+      <Row>
+        <Col xs={12} md={8}>
+        <h2>Playlist</h2>
+        <Auth />
+
+          {data.map((d)=>{
+          return(
+            <Cards image={d.album.images[0].url} title={d.album.name} artis={d.album.artists[0].name}/>    
+@@ -51,6 +52,7 @@ function App() {
+          )
+        })}
+        </Col>
+
+      </Row>
+      </Container>
+    </div>
