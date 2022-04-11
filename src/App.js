0804b3f9149503f98fc6cae1e7c2c7b81e25a48d
gugunmulyanaa <gugunmulyana7@gmail.com>
2022-04-11 12:45:03 +0700

    add button login

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import './App.css';
 import data from './Data/albums';
-import Cards from './Component/Card'; import List from './Component/List'; import Search from './Component/Search';
-import Auth from './Page/auth';
+import Cards from './Component/Card';
 import Container from 'react-bootstrap/esm/Container';
-import Col from 'react-bootstrap/Col'; import Row from 'react-bootstrap/Row';
-import {useState, useEfect} from 'react' ;
-import  axios from 'axios';
-// import  axios from 'axios';
-
-
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import List from './Component/List';
 function App() {
-
-  [accToken, setAccToken] = useState();
-  [accesSrcKey, setAccToken] = useState();
-  [accesTrack, setAccTrack] = useState();
-@@ -28,13 +27,15 @@ function App() {
-
   return (
     <div className="App">
-      <Auth />
+
+
+      <button>Login</button>
 
       <Container>
-
-      <Search />
       <Row>
         <Col xs={12} md={8}>
-        <h2>Playlist</h2>
-        <Auth />
-
-          {data.map((d)=>{
-          return(
-            <Cards image={d.album.images[0].url} title={d.album.name} artis={d.album.artists[0].name}/>    
-@@ -51,6 +52,7 @@ function App() {
-          )
-        })}
-        </Col>
-
-      </Row>
-      </Container>
-    </div>
